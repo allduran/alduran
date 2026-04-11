4a65c9c3281e5857e4f4e9ca513eb1bdef6cc74b
duran <cheshire.dd96@gmail.com>
2026-04-11 11:17:05 +0200

    Updates

---
+++ assets/mi_primera_guia-BdgConvW.js
@@ -0,0 +1,17 @@
+var e=`# Título de la guía\r
+\r
+Introducción: explica de qué va y qué aprenderá el lector.\r
+\r
+## Sección 1\r
+\r
+Contenido. Puedes usar \`código inline\` o bloques:\r
+\r
+\`\`\`js\r
+function ejemplo() {\r
+  return "hola mundo";\r
+}\r
+\`\`\`\r
+\r
+## Conclusión\r
+\r
+Resumen y siguientes pasos.`;export{e as default};
