58124eaace97168e5f6b549376c071900562416b
duran <cheshire.dd96@gmail.com>
2026-04-19 14:35:53 +0200

    Updates

---
+++ assets/mi_primera_guia-BY0esx4k.js
@@ -0,0 +1,17 @@
+var e=`# Título de la guía\r
+\r
+Introducción: Aquí explicaré de qué va el artículo y qué aprenderá el lector.\r
+\r
+## Sección 1\r
+\r
+Contenido. Puedo usar \`código inline\` o bloques:\r
+\r
+\`\`\`js\r
+function ejemplo() {\r
+  return "hola mundo";\r
+}\r
+\`\`\`\r
+\r
+## Conclusión\r
+\r
+Resumen y siguientes pasos.`;export{e as default};
